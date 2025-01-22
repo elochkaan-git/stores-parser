484a0f01f3e7e78083706533d8ed372472ae4070
elochkaan <elochkaan@gmail.com>
2025-01-22 22:50:21 +0700

    Все ссылки собраны, окончательные версии парсеров. Дальше только модификации и улучшения

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from bs4 import BeautifulSoup as bs
 import sqlite3
 import typing
@@ -10,6 +12,8 @@ import json
 import time
 import logging
 import multiprocessing
+import traceback
+import sys
 
 start = time.time()
 log = logging.getLogger('parser_logger')
@@ -35,6 +39,18 @@ def load_urls(path: str) -> list[str]:
         log.debug(f'Загружены ссылки из {path}')
         return json.load(file)
 
+def split_list(lst, n):
+    """Разбивает список lst на n примерно равных частей."""
+    avg = len(lst) / float(n)
+    out = []
+    last = 0.0
+
+    while last < len(lst):
+        out.append(lst[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
 
 def driver_initialization() -> webdriver.Firefox:
     log.info('Начинаем запускать браузер')
@@ -42,7 +58,6 @@ def driver_initialization() -> webdriver.Firefox:
     driver_options.add_argument("--headless")
     driver_service = Service(executable_path="/usr/bin/geckodriver")
     driver = webdriver.Firefox(options=driver_options, service=driver_service)
-    driver.implicitly_wait(10)
     log.info('Браузер настроен и готов к работе')
     return driver
 
@@ -79,18 +94,38 @@ def database_writer(queue: multiprocessing.Queue) -> None:
     connection.close()
 
 
+# def products_counter(driver: webdriver.Firefox, urls: typing.Dict[str, str]) -> int:
+#     counter = 0
+#     for index, url in urls.items():
+#         driver.get(url)
+#         soup = bs(driver.page_source, 'html.parser')
+#         counter += len(soup.select("[class*='akn2Ylc1S bkn2Ylc1S']"))
+#     return counter
+
+
 def adress_setup(adress: str, driver: webdriver.Firefox) -> None:
     driver.get("https://yarcheplus.ru/")
-    driver.find_element(By.XPATH, "//*[@class='a8MJ8NOjn e8MJ8NOjn aXjHckwsA t8MJ8NOjn n8MJ8NOjn l8MJ8NOjn']").click()
-    driver.find_element(By.XPATH, "//*[@id='receivedAddress']").send_keys("Томск, Учебная улица, 42")
-    driver.find_element(By.XPATH, "//*[@id='receivedAddress']").send_keys(Keys.ENTER)
-    driver.find_element(By.XPATH, "//*[@class='atLAAl6Nb gtLAAl6Nb']").click()
+
+    WebDriverWait(driver, 20).until(
+        ec.element_to_be_clickable((By.XPATH, '//*[@class="a8MJ8NOjn e8MJ8NOjn aXjHckwsA t8MJ8NOjn n8MJ8NOjn l8MJ8NOjn"]'))
+    ).click()
+
+    address_input = WebDriverWait(driver, 20).until(
+        ec.presence_of_element_located((By.XPATH, '//*[@id="receivedAddress"]'))
+    )
+    address_input.send_keys('Томск, Учебная улица, 42')
+    address_input.send_keys(Keys.ENTER)
+
+    WebDriverWait(driver, 20).until(
+        ec.element_to_be_clickable((By.XPATH, '//button[@class="atLAAl6Nb gtLAAl6Nb"]'))
+    ).click()
+
     log.info('Адрес установлен')
 
 
 def scraping(driver: webdriver.Firefox, urls: typing.Dict[str, str]) -> typing.Tuple[str, float, str, float, float, str, str]:
     log.info('Начинаем обрабатывать страницы')
-    for index, url in urls.items():
+    for url in urls:
         driver.get(url)
         log.debug(f'Обработка {url}')
         soup = bs(driver.page_source, 'html.parser')
@@ -114,19 +149,18 @@ def scraping(driver: webdriver.Firefox, urls: typing.Dict[str, str]) -> typing.T
             yield (name, value, unit, rating, cost, link, "Ярче!")
 
 
-def main(path_to_urls: str) -> typing.Tuple[str, float, str, float, float, str, str]:
+def main(urls: list[str]) -> typing.Tuple[str, float, str, float, float, str, str]:
     log.info('Программа запускается')
     results = []
     try:
-        logger_initialization(log)
         browser = driver_initialization()
         adress_setup('Томск, Учебная улица, 42', browser)
-        urls = load_urls(path_to_urls)
         for action in scraping(browser, urls):
             results.append(action)
     except Exception as e:
         log.error('Веб-драйвер отключен, база данных отключена. Программа завершила работу с ошибкой')
         log.error(e)
+        log.error(traceback.format_exc())
         browser.quit()
     else:
         browser.quit()
@@ -137,13 +171,15 @@ def main(path_to_urls: str) -> typing.Tuple[str, float, str, float, float, str, 
 
 if __name__ == "__main__":
     logger_initialization(log)
+    number_of_processes = int(sys.argv[1])
+    urls = split_list(load_urls('urls1.json'), number_of_processes)
     queue = multiprocessing.Queue()
 
     dbwriter = multiprocessing.Process(target=database_writer, args=(queue,))
     dbwriter.start()
 
-    with multiprocessing.Pool(processes=3) as pool:
-        for result in pool.imap(main, ['urls1.json', 'urls2.json', 'urls3.json']):
+    with multiprocessing.Pool(processes=number_of_processes) as pool:
+        for result in pool.imap(main, urls):
             for value in result:
                 queue.put(value)
 
