1c5d152e38473649787cae218ba1f3a878b87391
elochkaan <elochkaan@gmail.com>
2025-01-28 18:24:18 +0700

    1. Добавлен импорт TerminalMenu из simple_term_menu 2. Импорт класса Text из rich.text 3. Прямой импорт классов List, Tuple и Dict из модуля typing, поэтому везде удалено typing. 4. Добавлены константы, отвечающие за id магазинов и сообщения 5. Изменена аннотация функции load_urls(): теперь она возвращает словарь 6. Теперь функция set_adress_5ka() может устанавливать пользовательский адрес (будьте аккуратнее!) 7. Вызываемые функции в функции worker() теперь зависят от id выбранного магазина 8. Если число процессов превышает количество выбранных ссылок, то оно уменьшается до количества ссылок 9. Теперь можно самому выбирать, какие категории товаров ты хочешь парсить 10. Добавлены цветные предупреждения

--- merged_parser.py
+++ merged_parser.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from simple_term_menu import TerminalMenu
 from prompt_toolkit import prompt
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
@@ -9,8 +10,9 @@ from selenium.webdriver.support import expected_conditions as ec
 from bs4 import BeautifulSoup as bs
 from rich.console import Console
 from rich.table import Table
+from rich.text import Text
 import sqlite3
-import typing
+from typing import List, Tuple, Dict
 import json
 import time
 import logging
@@ -19,6 +21,19 @@ import traceback
 import sys
 import os
 import re
+
+YARCHEPLUS = 1
+PYATERKA = 2
+WARNING = ('NOTE: Обязательный параметр!',
+           'Адрес задается в формате "Город, улица, номер дома"',
+           'Но будьте внимательны, адрес у Пятерочки очень чувствительный',
+           'То есть "Улица Красноармейская" и "Красноармейская улица" для их сайта -- разные улицы',
+           'Поэтому советуем перед тем, как задать адрес, зайти на сайт каталога Пятерочки,',
+           'вручную ввести адрес, чтобы убедиться в его правильности, а потом скопировать в программу.',
+           'Например, я ввожу адрес "Томск, улица Красноармейская, 114" на сайте магазина, выбираю',
+           'первый адрес в списке, который выглядит как "Красноармейская улица, 114, Россия, Томск".',
+           'Ага, значит в эту программу мне нужно внести "Томск, Красноармейская улица, 114"')
+MESSAGE = 'NOTE: если вы не хотите менять какой-то параметр, то просто нажмите Enter'
 
 
 start = time.time()
@@ -39,13 +54,13 @@ def logger_initialization(logger: logging.Logger) -> None:
     logger.addHandler(file_handler)
 
 
-def load_urls(path: str) -> list[str]:
+def load_urls(path: str) -> Dict[str, str]:
     with open(path, 'r') as file:
         log.debug(f'Загружены ссылки из {path}')
         return json.load(file)
 
 
-def split_list(dict: typing.Dict[str, str], n: int) -> typing.List[typing.List[str], ...]:
+def split_list(dict: Dict[str, str], n: int) -> list[list[str], ...]:
     """Разбивает список lst на n примерно равных частей."""
     lst = list(dict.values())
     avg = len(lst) / float(n)
@@ -137,11 +152,12 @@ def adress_setup_5ka(adress: str, driver: webdriver.Firefox) -> None:
         adress_input = WebDriverWait(driver, 20).until(
             ec.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[3]/div/section/div/div[2]/div/div[2]/div/input'))
         )
-        for letter in 'Томск, Красноармейская улица, 114':
+        for letter in adress:
             adress_input.send_keys(letter)
 
+        adress = adress.split(' ', 1)[1]
         WebDriverWait(driver, 20).until(
-            ec.element_to_be_clickable((By.XPATH, f'//p[contains(text(), {adress})]'))
+            ec.element_to_be_clickable((By.XPATH, f'//p[contains(text(), "{adress}")]'))
         ).click()
 
         button_accept = WebDriverWait(driver, 20).until(
@@ -157,7 +173,7 @@ def adress_setup_5ka(adress: str, driver: webdriver.Firefox) -> None:
     log.info(f'Адрес установлен для браузера в процессе {multiprocessing.current_process().pid}')
 
 
-def scraping_yarcheplus(driver: webdriver.Firefox, urls: typing.Dict[str, str]) -> typing.Tuple[str, float, str, float, float, str, str]:
+def scraping_yarcheplus(driver: webdriver.Firefox, urls: list[str]) -> Tuple[str, float, str, float, float, str, str]:
     log.info('Начинаем обрабатывать страницы')
     WebDriverWait(driver, 20).until(
         ec.presence_of_element_located((By.XPATH, '//*[@class="loFy5xub4 WoFy5xub4 coFy5xub4"]'))
@@ -192,7 +208,7 @@ def scraping_yarcheplus(driver: webdriver.Firefox, urls: typing.Dict[str, str]) 
             log.error(traceback.format_exc())
 
 
-def scraping_5ka(driver: webdriver.Firefox, urls: typing.List[str]) -> typing.Tuple[str, float, str, float, float, str, str]:
+def scraping_5ka(driver: webdriver.Firefox, urls: List[str]) -> Tuple[str, float, str, float, float, str, str]:
     time.sleep(3)
     log.info('Начинаем обрабатывать страницы')
     log.debug(f'Текущий адрес: {driver.find_element(By.XPATH, '//p[@class="chakra-text fyOFNehN- SdFzIDV1- css-15mul6p"]').text}')
@@ -226,14 +242,20 @@ def scraping_5ka(driver: webdriver.Firefox, urls: typing.List[str]) -> typing.Tu
             yield (name, value, unit, rating, cost, link, "Пятерочка")
 
 
-def worker(args: typing.Tuple[list[str], str]) -> typing.Tuple[str, float, str, float, float, str, str]:
+def worker(args: Tuple[list[str], str, int]) -> Tuple[str, float, str, float, float, str, str]:
     log.info('Программа запускается')
     results = []
     try:
         browser = driver_initialization()
-        adress_setup(args[1], browser)
-        for action in scraping(browser, args[0]):
-            results.append(action)
+        if args[2] == YARCHEPLUS:
+            adress_setup_yarcheplus(args[1], browser)
+            for action in scraping_yarcheplus(browser, args[0]):
+                results.append(action)
+        elif args[2] == PYATERKA:
+            adress_setup_5ka(args[1], browser)
+            for action in scraping_5ka(browser, args[0]):
+                results.append(action)
+        
     except:
         log.error(f'Браузер отключен, процесс {multiprocessing.current_process().pid} завершен с ошибкой')
         log.error(traceback.format_exc())
@@ -244,15 +266,18 @@ def worker(args: typing.Tuple[list[str], str]) -> typing.Tuple[str, float, str, 
         return results
 
 
-def main(options: list[int, str, str, str]) -> None:
-    number_of_processes = int(options[0])
-    urls = split_list(load_urls(options[1]), number_of_processes)
+def main(options: Dict[str, str]) -> None:
+    number_of_processes = int(options['number_of_processes'])
+    urls = split_list(options['selected_urls'], number_of_processes)
+    if [] in urls:
+        number_of_processes = len(urls) - urls.count([])
+        urls = [i for i in urls if i != []]
     queue = multiprocessing.Queue()
 
-    dbwriter = multiprocessing.Process(target=database_writer, args=(queue, options[3],))
+    dbwriter = multiprocessing.Process(target=database_writer, args=(queue, options['name_of_table'],))
     dbwriter.start()
 
-    args = [(part_of_urls, options[2]) for part_of_urls in urls]
+    args = [(part_of_urls, options['adress'], int(options['store_id'])) for part_of_urls in urls]
     with multiprocessing.Pool(processes=number_of_processes) as pool:
         for result in pool.imap(worker, args):
             for value in result:
@@ -263,7 +288,7 @@ def main(options: list[int, str, str, str]) -> None:
     log.info(f"Время выполнения составило {time.time() - start} секунд")
 
 
-def menu() -> list[int, str, str, str]:
+def menu() -> list[int, str, str, str, int]:
 
     console = Console()
 
@@ -288,34 +313,70 @@ def menu() -> list[int, str, str, str]:
         store = prompt('Какой магазин выбираем? (введите номер): ')
 
         options = []
+        urls = {}
         if store == 'q':
             sys.exit()
         elif int(store) == 1:
-            options = [3, './urls1.json', '', 'Products_yarcheplus']
+            options = {
+                'number_of_processes' : '3',
+                'path_to_urls' : './urls1.json',
+                'adress' : '',
+                'name_of_table' : 'Products_yarcheplus',
+                'store_id' : str(YARCHEPLUS),
+                'selected_urls' : {}
+            }
         elif int(store) == 2:
-            options = [3, './test_urls1.json', '', 'Products_5ka']
+            options = {
+                'number_of_processes' : '3',
+                'path_to_urls' : './test_urls1.json',
+                'adress' : '',
+                'name_of_table' : 'Products_5ka',
+                'store_id' : str(PYATERKA),
+                'selected_urls' : {}
+            }
 
-        is_default = prompt('Хотите изменить базовые настройки? (ввести адрес магазина можно будет при любом ответе) [y/n]: ')
+        is_default = prompt('Хотите изменить базовые настройки? [y/n]: ')
         if is_default.lower() == 'y':
-            answer = prompt('Введите количество процессов (по умолчанию 1): ') # Number of processes
+
+            console.print(Text(MESSAGE, style='italic yellow', justify='full'))
+
+            answer = prompt('Введите количество процессов: ') # Number of processes
             if answer != '':
-                options[0] = answer
+                options['number_of_processes'] = answer
 
             answer = prompt('Введите путь до ссылок: ') # Path to urls
             if answer != '':
-                options[1] = answer
-
+                options['path_to_urls'] = answer
+            
+            console.print(Text(' '.join(WARNING), style='italic yellow', justify='full'))
             answer = prompt('Введите адрес магазина: ') # Adress of store
             if answer != '':
-                options[2] = answer
+                options['adress'] = answer
 
             answer = prompt('Введите название таблицы в базе данных, в которую хотите сохранить продукты: ') # Name of table in db
             if answer != '':
-                options[3] = answer
+                options['name_of_table'] = answer
         else:
-            options[2] = prompt('Введите адрес магазина: ') # Adress of store
+            console.print(Text(MESSAGE, style='italic yellow', justify='full'))
+            console.print(Text(' '.join(WARNING), style='italic yellow', justify='full'))
+            options['adress'] = prompt('Введите адрес магазина: ') # Adress of store
 
-        print()
+        urls = load_urls(options['path_to_urls'])
+        options['selected_urls'] = urls
+        answer = prompt('Хотите выбрать конкретные категории для парсинга? [y/n]: ')
+        if answer.lower() == 'y':
+            term_menu = TerminalMenu(
+                urls,
+                multi_select=True
+            )
+
+            term_menu.show()
+            new_urls = {}
+            for item in term_menu.chosen_menu_entries:
+                new_urls[item] = urls[item]
+            options['selected_urls'] = new_urls
+
+        console.print()
         answer = prompt('Начинаем парсинг или желаете что-то изменить? (программа начнет работу с выбора магазина) [y/n]: ')
         if answer.lower() == 'y':
             break
